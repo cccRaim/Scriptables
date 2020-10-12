82294451f830096977b9c55e1c4f959ac745e927
陈然 <201319630201@zjut.edu.cn>
2020-10-12 21:22:29 +0800

    Update latest.js

--- webview/latest.js
+++ webview/latest.js
@@ -45,8 +45,8 @@ class Im3xWidget {
    * 渲染大尺寸组件
    */
   async renderLarge () {
-    let w = new ListWidget()
-    w.addText("不支持尺寸")
+    const w = new Webview()
+    w.getHTML('https://www.google.com/')
     return w
   }
 
